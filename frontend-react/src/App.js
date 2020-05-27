e65f038947eec34dff9a2384a3993265a2f5e44c
Michelle <Michelle.Tinajero@outlook.com>
2020-05-26 22:19:36 -0500

    added private route and set login auth

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
-import { Route, Link } from "react-router-dom";
+import { Route, Link, BrowserRouter} from "react-router-dom";
 
 import UserLogin from "./components/UserLogin";
 import UserSignUp from "./components/UserSignUp";
 import PlantForm from "./components/PlantForm";
+
+//unit3 react
+import PrivateRoute from './Unit3-React-Folder/PrivateRoute';
+import HomePage from './Unit3-React-Folder/components/home-page';
+//unit3 react ^^^^
 
 import "./App.css";
 import "./index.css";
@@ -11,28 +16,31 @@ import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <Link to="/">
-        <button>Home</button>
-      </Link>
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">
+          <button>Home</button>
+        </Link>
 
-      <Link to="/Login">
-        <button>Login</button>
-      </Link>
+        <Link to="/Login">
+          <button>Login</button>
+        </Link>
 
-      <Link to="/Register">
-        <button>Sign Up</button>
-      </Link>
+        <Link to="/Register">
+          <button>Sign Up</button>
+        </Link>
 
-      <Link to="/PlantForm">
-        <button>Add your plant!</button>
-      </Link>
+        <Link to="/PlantForm">
+          <button>Add your plant!</button>
+        </Link>
 
-      {/* Route exact path="/" components={Home} /> */}
-      <Route path="/Login" component={UserLogin} />
-      <Route path="/Register" component={UserSignUp} />
-      <Route path="/PlantForm" component={PlantForm} />
-    </div>
+        {/* Route exact path="/" components={Home} /> */}
+        <Route path="/Login" component={UserLogin} />
+        <PrivateRoute path='/home-page' component={HomePage} />
+        <Route path="/Register" component={UserSignUp} />
+        <Route path="/PlantForm" component={PlantForm} />
+      </div>
+    </BrowserRouter>
   );
 }
 
