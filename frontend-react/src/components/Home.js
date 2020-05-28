fb89c9a1d8659e8144ad6234182f44d83a3295e3
Michelle Hey <miscmiche@gmail.com>
2020-05-27 23:51:03 -0400

    Changes for now. Will need to correct axios call to display username and phonenumber.

--- frontend-react/src/components/Home.js
+++ frontend-react/src/components/Home.js
@@ -1,14 +1,13 @@
 import React from "react";
 // import axios from "axios";
-import * as yup from "yup";
 import "../index.css";
 
-
+// Michelle React 3 may put something in here
 
 const Home = () => {
 
     return (
-
+        <h1>Welcome to the Water My Plants App!</h1>
     );
 }
 
--- frontend-react/src/components/UserProfile.js
+++ frontend-react/src/components/UserProfile.js
@@ -1,13 +1,38 @@
+import React from "react";
+import axios from "axios";
+
 // get request to post info using https://reqres.in/api/users
+// goal is to get the user information
 
-axios.get('https://water-my-plants-backend-vw.herokuapp.com//users')
-  .then( response => {
-    console.log(response);
-    const data = response.data;
-    // const cardDiv = document.querySelector('.cards');
-    // cardDiv.appendChild(createMyCard(data));
-  })
-  .catch( err => {
-    console.log("Error: ", err);
-  })
+const UserProfile = (props) => {
+    const [info, setInfo] = useState([]);
+    console.log(info);
+  
+    useEffect(() => {
+      axios
+        .get("https://water-my-plants-backend-vw.herokuapp.com/user")
+        .then(res => 
+            console.log(res);
+            setInfo(res.data))
+        .catch(err => console.log(err));
+    }, []);
 
+    return (
+      <div className="userDiv">
+        <h1>User Information</h1>
+        <div className="cardContainer">
+            {props.user.map(user => (
+                    <div key={user.id} className='matesCard'>
+                        <p>username: {user.username}</p>
+                        <p>password: {user.password}</p>
+                        <p>phoneNumber: {user.phoneNumber}</p>
+                    </div>
+            )};
+        </div>
+      </div>
+    );
+};
+
+           
+export default UserProfile;
+  
