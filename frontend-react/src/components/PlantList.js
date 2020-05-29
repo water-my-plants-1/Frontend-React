45d92ecd2abce87d8fb8883700b7c433e439591c
Evelyn Nguyen <Evelyn@Viets-iMacx.local>
2020-05-29 12:11:25 -0700

    redo w.api

--- frontend-react/src/components/PlantList.js
+++ frontend-react/src/components/PlantList.js
@@ -1,29 +1,25 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { axiosWithAuth } from '../Unit3-React-Folder/utils/axiosWithAuth';
 
 import "../index.css";
-
-
 
 const PlantList = () => {
     const [listState, setListState] =useState([]);
 
-    axios
-    .get("https://water-my-plants-backend-vw.herokuapp.com/user/plants")
-   
-    .then(response => {
-      console.log(response.data);
-      setListState(response.data);
-      //update display plants state
-      // props.newPlant(response.data);
-      // props.history.push("/PlantList");
-      
-    })
-    .catch(err => console.log(err));
-
     const removePlant = plant => {
         setListState(listState.filter(item => item.nickname !== plant.nickname));
       };
+
+    useEffect(() => {
+        axiosWithAuth()
+            .get("https://water-my-plants-backend-vw.herokuapp.com/user/plants")
+        
+            .then(response => {
+                setListState(response.data);
+        
+        })
+        .catch(err => console.log(err));
+    }, [])
 
 
     return(
@@ -36,7 +32,7 @@ const PlantList = () => {
                             <div className="plant" key={i}>
                                 <h3>Plant Name: {plant.nickname}</h3>
                                 <h3>Plant Species: {plant.species}</h3>
-                                <h3>Please water : {plant.H2oFrequency}</h3>
+                                <h3>Please water : {plant.h2oFrequency}</h3>
                                 <button onClick={() => removePlant(plant)}>Remove Plant</button>
                             </div>
                         );
